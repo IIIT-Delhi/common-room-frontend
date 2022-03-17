b0a1bac0dd7adcd35c476292aa684669aa6febf1
Abhimanyu Jha <abhimanyu.in@gmail.com>
2022-03-17 23:36:57 +0530

    bottom nav fix

--- src/navigation/home/index.tsx
+++ src/navigation/home/index.tsx
@@ -11,43 +11,50 @@ function BottomTabs() {
 	const { colors } = useTheme();
 	return (
 		<Tab.Navigator
-			screenOptions={{
+			screenOptions={({ route }) => ({
 				headerShown: false,
 				tabBarStyle: {
 					backgroundColor: colors.bg['500'],
+					height: 65,
+					paddingBottom: 5,
+					paddingTop: 5,
+					elevation: 0, // remove shadow on Android
+					shadowOpacity: 0, // remove shadow on iOS
+					borderTopColor: '#1e1e1e',
+					borderTopWidth: 2,
 				},
-			}}
+				tabBarIcon: ({ focused }) => {
+					const icons = {
+						Feed: focused ? 'ri-home-2-fill' : 'ri-home-2-line',
+						Notifications: focused
+							? 'ri-notification-2-fill'
+							: 'ri-notification-2-line',
+						Explore: focused
+							? 'ri-search-2-fill'
+							: 'ri-search-2-line',
+						Account: focused
+							? 'ri-account-circle-fill'
+							: 'ri-account-circle-line',
+					};
+					const iconName = icons[route.name];
+					const size = focused ? '7' : '7';
+					return (
+						<Icon
+							as={RemixIcon}
+							key={iconName}
+							name={iconName}
+							size={size}
+							color={focused ? 'primary.500' : 'subtle.500'}
+						/>
+					);
+				},
+				tabBarLabel: () => null,
+			})}
 		>
-			<Tab.Screen
-				name="Feed"
-				component={FeedScreen}
-				options={{
-					tabBarLabel: 'Home',
-					tabBarIcon: ({ focused, color, size }) => (
-						<Icon
-							as={RemixIcon}
-							name={`home-2-${focused ? 'fill' : 'line'}`}
-							color={color}
-							size={size}
-						/>
-					),
-				}}
-			/>
-			<Tab.Screen
-				name="Notifications"
-				component={NotificationsScreen}
-				options={{
-					tabBarLabel: 'Notifications',
-					tabBarIcon: ({ focused, color, size }) => (
-						<Icon
-							as={RemixIcon}
-							name={`notification-2-${focused ? 'fill' : 'line'}`}
-							color={color}
-							size={size}
-						/>
-					),
-				}}
-			/>
+			<Tab.Screen name="Feed" component={FeedScreen} />
+			<Tab.Screen name="Notifications" component={NotificationsScreen} />
+			<Tab.Screen name="Explore" component={NotificationsScreen} />
+			<Tab.Screen name="Account" component={NotificationsScreen} />
 		</Tab.Navigator>
 	);
 }
