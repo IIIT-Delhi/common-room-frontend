51416f51466e184004cd02670872e15ffbf6fdd4
Abhimanyu Jha <abhimanyu.in@gmail.com>
2022-03-21 18:37:10 +0530

    homestack

--- App.tsx
+++ App.tsx
@@ -2,15 +2,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider } from './src/providers';
 import HomeStack from './src/navigation/home';
-import { EventScreen } from './src/screens';
 
 export default function App() {
 	return (
 		<ThemeProvider>
 			<SafeAreaProvider>
 				<NavigationContainer>
-					{/* <HomeStack /> */}
-					<EventScreen />
+					<HomeStack />
 				</NavigationContainer>
 			</SafeAreaProvider>
 		</ThemeProvider>
