f1bae9b00fd97872d1f6231be4994603298d1ecc
Abhimanyu Jha <abhimanyu.in@gmail.com>
2022-04-29 17:32:40 +0530

    weird and non weird parentscroll

--- src/components/general/ParentScrollContainer.tsx
+++ src/components/general/ParentScrollContainer.tsx
@@ -1,4 +1,4 @@
-import { Box, FlatList, StatusBar, useTheme } from 'native-base';
+import { Box, FlatList, StatusBar, ScrollView, useTheme } from 'native-base';
 import { useState } from 'react';
 import { RefreshControl, ScrollViewProps } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -9,7 +9,7 @@ type ParentScrollContainerProps = ScrollViewProps & {
 	contentUnderStatusBar?: boolean;
 };
 
-function ParentScrollContainer({
+function ParentScrollContainerWeird({
 	children,
 	showsVerticalScrollIndicator = true,
 	noHorizontalPadding = false,
@@ -66,4 +66,59 @@ function ParentScrollContainer({
 	);
 }
 
-export default ParentScrollContainer;
+// import { Box, ScrollView, StatusBar, useTheme } from 'native-base';
+// import { useState } from 'react';
+// import { RefreshControl, ScrollViewProps } from 'react-native';
+// import { SafeAreaView } from 'react-native-safe-area-context';
+// import { useQueryClient } from 'react-query';
+
+function ParentScrollContainer({
+	children,
+	showsVerticalScrollIndicator = true,
+	noHorizontalPadding = false,
+	contentUnderStatusBar = false,
+	...props
+}: ParentScrollContainerProps) {
+	const { colors } = useTheme();
+	const queryClient = useQueryClient();
+	const [refreshing, setRefreshing] = useState(false);
+	const onRefresh = async () => {
+		setRefreshing(true);
+		await queryClient.invalidateQueries();
+		setRefreshing(false);
+	};
+
+	return (
+		<SafeAreaView style={{ flex: 1 }}>
+			<Box flex="1">
+				<StatusBar
+					barStyle="light-content"
+					backgroundColor={
+						contentUnderStatusBar ? 'transparent' : colors.bg['500']
+					}
+					translucent={contentUnderStatusBar}
+				/>
+				<ScrollView
+					bg="bg.500"
+					px={noHorizontalPadding ? '0' : '4'}
+					{...props}
+					contentContainerStyle={{
+						paddingBottom: 40,
+					}}
+					showsVerticalScrollIndicator={showsVerticalScrollIndicator}
+					refreshControl={
+						<RefreshControl
+							refreshing={refreshing}
+							onRefresh={onRefresh}
+						/>
+					}
+					nestedScrollEnabled
+				>
+					{children}
+				</ScrollView>
+			</Box>
+		</SafeAreaView>
+	);
+}
+
+export default ParentScrollContainerWeird;
